8f476a9d368d14bebb76b5428379dc7e689977ca
Garrett Wright <garrettwrong@gmail.com>
2020-05-10 22:14:07 -0400

    Bump version: 0.2.0 → 0.3.0

--- cuTWED/__init__.py
+++ cuTWED/__init__.py
@@ -2,4 +2,4 @@ from .cuTWED import twed
 from .cuTWED import twed_dev
 
 __all__ = ["twed", "twed_dev"]
-__version__ = '0.2.0''
+__version__ = '0.3.0''
--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ if 'TOXENV' in os.environ and 'SETUPPY_CFLAGS' in os.environ:
 
 setup(
     name='cuTWED',
-    version='0.2.0',
+    version='0.3.0',
     description='A linear memory CUDA Time Warp Edit Distance algorithm.',
     long_description=long_description,
     long_description_content_type='text/markdown',
