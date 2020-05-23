d0795b04e9848570bc826503bccea74f9fe891a3
Garrett Wright <garrettwrong@gmail.com>
2020-05-23 10:48:08 -0400

    get ref py bindings working with basic test

--- cuTWED/__init__.py
+++ cuTWED/__init__.py
@@ -1,6 +1,6 @@
-from .CTWED import CTWED
+from .CTWED import ctwed
 from .cuTWED import twed
 from .cuTWED import twed_dev
 
-__all__ = ["twed", "twed_dev", 'CTWED']
+__all__ = ["twed", "twed_dev", 'ctwed']
 __version__ = '0.3.1'
